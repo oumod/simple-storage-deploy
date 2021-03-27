e00c1626d0bfc70cfbb6a138b2c431cdd2b9ff0c
Oudom Suos <oudom.suos@gmail.com>
2021-03-27 14:18:25 +0100

    Updates

---
+++ precache-manifest.191e20e68574755648ddb0ddeed4212b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4f6725f1716f16ffb7a83122890b28",
+    "url": "/simple-storage-deploy/index.html"
+  },
+  {
+    "revision": "e9713b8f54677e202ca7",
+    "url": "/simple-storage-deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3147866c7f1010dfc310",
+    "url": "/simple-storage-deploy/static/js/2.b3b3570b.chunk.js"
+  },
+  {
+    "revision": "e9713b8f54677e202ca7",
+    "url": "/simple-storage-deploy/static/js/main.aa9fd057.chunk.js"
+  },
+  {
+    "revision": "8293ebc536f8a3a1d83c",
+    "url": "/simple-storage-deploy/static/js/runtime-main.f5e89d9c.js"
+  }
+]);
